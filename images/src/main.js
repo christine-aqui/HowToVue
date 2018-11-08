123aa27865d8774bd10aaf32dd3d62e82770019a
Chris <aqui.christine@gmail.com>
2018-11-07 22:52:52 -0500

    connected vuex to vue

--- images/src/main.js
+++ images/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 
 //  createElement was shorten to h
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app');
